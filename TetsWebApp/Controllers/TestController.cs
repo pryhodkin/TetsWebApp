0ad2fd2e5113303981b10f9adfedbeb38a070798
Yaroslav Prykhodko <you@example.com>
2021-08-19 09:39:59 +0300

    Add project files.

---
+++ TetsWebApp/Controllers/TestController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TetsWebApp.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TetsWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public static int GetNewId() => id++;
+        private static int id = 1;
+
+        public static List<TestEntity> List = new List<TestEntity>()
+        {
+            new TestEntity
+            {
+                Id = GetNewId(),
+                Name = "New Entity",
+                Created = DateTime.Now
+            }
+        };
+
+
+        // GET: api/<TestController>
+        [HttpGet]
+        public IEnumerable<TestEntity> Get()
+        {
+            return List;
+        }
+
+        // GET api/<TestController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TestEntity>> Get(int id)
+        {
+            var entity = List.FirstOrDefault(x => x.Id == id);
+            if (entity is null)
+                return NotFound();
+
+            return entity;
+        }
+
+        // POST api/<TestController>
+        [HttpPost]
+        public async Task<ActionResult<int>> Post([FromBody] TestEntity value)
+        {
+            value.Id = GetNewId();
+            value.Created = DateTime.Now;
+            List.Add(value);
+            return value.Id;
+        }
+
+        // PUT api/<TestController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] TestEntity value)
+        {
+            if(id != value.Id)
+                return BadRequest();
+
+            if (!List.Any(x => x.Id == id))
+                return NotFound();
+
+            List.RemoveAll(x => x.Id == id);
+            List.Add(value);
+
+            return NoContent();
+        }
+
+        // DELETE api/<TestController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (!List.Any(x => x.Id == id))
+                return NotFound();
+
+            List.RemoveAll(x => x.Id == id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ TetsWebApp/Models/TestEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TetsWebApp.Models
+{
+    public class TestEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
